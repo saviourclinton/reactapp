d3d3158c0e68887e81346e4ea13738cc586dcd42
saviourclinton <63399794+saviourclinton@users.noreply.github.com>
2020-09-05 10:22:53 +0530

    counter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 
 import ReactDOM from "react-dom";
 
-function Appy() {
-  return (
-    <h1>
-      {" "}
-      Hello World We are <span>Happy in react world</span>
-    </h1>
-  );
-}
-ReactDOM.render(<Appy />, document.getElementById("root"));
+import App from './App'
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
